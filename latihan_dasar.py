ce8a77346291bfa75ddd35d92f0a558e20b6577c
ridwanbmkg <muhammad.ridwan@bmkg.go.id>
2025-07-20 00:29:35 +0700

    Menambah file latihan_dasar.py

---
+++ latihan_dasar.py
@@ -0,0 +1,7 @@
+# latihan_dasar.py
+
+print("Program Konversi Suhu Sederhana")
+celsius = float(input("Masukkan suhu dalam Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius} derajat Celsius sama dengan {fahrenheit} derajat Fahrenheit.")
